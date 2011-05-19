154df64ee0eb03938a5b20124e615d66d37cec5e
nano.seabra <nano.seabra@abb2a778-013e-ceb8-0ac5-c21ffb4112cc>
2011-05-19 15:25:37 +0000

    - A construção do grafo com as arestas de desejo foi feita - A construção do grafo com as arestas de realidade ainda estão pela metade

--- trunk/main.c
+++ trunk/main.c
@@ -1,14 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
 
-char *input;
-long file_len;
+int n_input;
 
-int **reality_matrix;
-int **desire_matrix;
+signed int *sequence;
+signed int *reality_graph;
+signed int *desire_graph;
 
 void readInput(char * filename)
 {
+    int i;
+    int j = 0;
+    int n_spaces = 0;
+    long file_len;
+    char *input;
+    char gene[5];
+    char s_gene;
+    char temp[2];
     FILE *file = fopen(filename, "r");
 
     if (file == 0)
@@ -17,10 +27,12 @@ void readInput(char * filename)
         exit(1);
     }
 
+    // checking input file size
     fseek(file, 0, SEEK_END);
     file_len = ftell(file);
     rewind(file);
 
+    // allocating memory for the input
     input = (char*)calloc(file_len + 1, sizeof(char));
 
     if (input == NULL)
@@ -29,16 +41,59 @@ void readInput(char * filename)
         exit(1);
     }
 
+    // getting the input
     fread(input, file_len, 1, file);
     input[file_len] = '\0';
 
     fclose(file);
+
+    // counting the number of spaces
+    for (i = 0; input[i] != '\0'; i++)
+    {
+        if (isspace(input[i]))
+            n_spaces++;
+    }
+
+    // input size is the number of spaces plus one
+    n_input = n_spaces++;
+
+    // getting the sequence from the input
+    //sequence = (signed int*)calloc(n_input, sizeof(signed int));
+    //for (i = 0; input[i] != '\0'; i++)
+    //{
+    //    if (!isspace(input[i]))
+    //        strcat(gene, input[i]);
+    //    else
+    //    {
+    //        temp[0] = gene;
+    //        temp[1] = '\0';
+    //        strcat(s_gene, temp);
+    //        sequence[j] = atoi(s_gene);
+    //        j++;
+    //    }
+    //}
 }
 
 void createDiagram()
 {
-    // guardar diagrama
-    free(input);
+    int i;
+
+    // allocating memmory for the graphs
+    reality_graph = (int*)calloc(2*n_input + 2, sizeof(signed int));
+    desire_graph = (signed int*)calloc(2*n_input + 2, sizeof(signed int));
+
+    // creating desire edges
+    for (i = 0; i != 2*n_input + 2; i++)
+    {
+        if (i%2 == 0)
+            desire_graph[i] = -(i/2 + 1);
+        else
+            desire_graph[i] = (i-1)/2;
+    }
+
+    // creating reality edges
+    
+
 }
 
 int main(int argc, char *argv[])

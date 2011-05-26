9c2898b6672cb62920ad6cb0ded5e339ec4ab03f
nano.seabra <nano.seabra@abb2a778-013e-ceb8-0ac5-c21ffb4112cc>
2011-05-26 03:24:13 +0000

    - Criação da função findCycle, que procura um ciclo, dado uma posição inicial - Criação da função findAllCycles, que busca por todos os ciclos do diagrama - Criação de novas estruturas:   . cycle_id   . cycles (ainda não é variável global)

--- main.c
+++ main.c
@@ -7,6 +7,7 @@
 
 int n_input;
 int *position;
+int **cycle_id;
 signed int *sequence;
 signed int **reality_graph;
 signed int **desire_graph;
@@ -62,7 +63,7 @@ void readInput(char *filename)
 
     element = (char *)calloc(n_input, sizeof(char));
     sequence = (signed int *)calloc(n_input, sizeof(signed int));
-    position = (int *)calloc(n_input + 1, sizeof(int));
+    position = (int *)calloc(n_input + 2, sizeof(int));
 
     // converting to signed int array
     // +i means that i's orientation is -->
@@ -71,7 +72,7 @@ void readInput(char *filename)
     last_space = -1;
     for (i = 0; input[i] != '\0'; i++)
     {
-        if (isspace(input[i]))
+        if (isspace(input[i]) || (i == file_len - 1))
         {
             for (j = 0; j < (i - last_space); j++)
                 element[j] = input[last_space + j + 1];
@@ -83,8 +84,7 @@ void readInput(char *filename)
         }
     }
 
-    i++;
-    position[abs(atoi(element))] = counter;
+    position[n_input + 1] = n_input + 1;
 
     free(input);
     free(element);
@@ -155,8 +155,16 @@ void createRealityGraph()
     // reality_graph[i][1] stores the gene which +i is connected with, considering reality edges
     reality_graph = (signed int **)malloc((n_input + 2) * sizeof(*reality_graph));
     
+    // cycle_id is a matrix (n_input + 2) by 2
+    // cycle_id[i][0] stores the cycle id where the edge (-i, reality_graph[i][0]) is, plus 1
+    // cycle_id[i][1] stores the cycle id where the edge (+i, reality_graph[i][1]) is, plus 1
+    cycle_id = (int **)calloc(n_input + 2, sizeof(*cycle_id));
+    
     for (i = 0; i <= n_input + 2; i++)
+    {
         reality_graph[i] = malloc(2 * sizeof(signed int));
+        cycle_id[i] = calloc(2, sizeof(int));
+    }
 
     reality_graph[0][0] = INT_MAX;
     next = 0;
@@ -288,12 +296,132 @@ void revert(int i, int len)
     updateRealityGraph(i, len);
 }
 
+signed int **findCycle(int initial_pos, int id)
+{
+    int i;
+    int j;
+    signed int current_pos;
+
+    // 'cycle' is the array for one cycle
+    // cycle[i] stores a reality edge of this cycle
+    // ex.: cycle[i][0] stores one node of the edge
+    //      cycle[i][1] stores another node of the edge
+    //      cycle[i][2] stores whether the edge is oriented from left to right (1) or from right to left (-1)
+    // as we do not know initially how many reality edges the cycle has, we first use 'temp_cycle', and then,
+    // we create 'cycle' with the correct size
+    signed int **cycle;
+    signed int **temp_cycle;
+
+    temp_cycle = (signed int **)calloc(n_input + 1, sizeof(*temp_cycle));
+    for (i = 0; i != (n_input + 1); i++)
+    {
+        temp_cycle[i] = calloc(3, sizeof(signed int));
+    }
+
+    i = 0;
+    j = 0;
+    current_pos = initial_pos;
+    while (i != 1)
+    {
+        // reality graph
+        temp_cycle[j][0] = current_pos;
+        if (current_pos >= 0)
+        {
+            temp_cycle[j][1] = reality_graph[current_pos][1];
+            cycle_id[current_pos][1] = id;
+        }
+        else
+        {
+            temp_cycle[j][1] = reality_graph[-current_pos][0];
+            cycle_id[-current_pos][0] = id;
+        }
+
+        if (temp_cycle[j][1] > 0)
+            cycle_id[temp_cycle[j][1]][1] = id;
+        else
+            cycle_id[-temp_cycle[j][1]][0] = id;
+        
+        if (position[abs(temp_cycle[j][0])] < position[abs(temp_cycle[j][1])])
+            temp_cycle[j][2] = 1;
+        else
+            temp_cycle[j][2] = -1;
+
+        // desire graph
+        current_pos = temp_cycle[j][1];
+        if (current_pos >= 0)
+            current_pos = desire_graph[current_pos][1];
+        else
+            current_pos = desire_graph[-current_pos][0];
+
+        if (current_pos == initial_pos)
+            i++;
+
+        j++;
+    }
+
+    cycle = (signed int **)calloc(j, sizeof(*cycle));
+    for (i = 0; i != j; i++)
+        cycle[i] = temp_cycle[i];
+
+    free(temp_cycle);
+
+    printf("\n\nCycle:\n");
+    printf("[ ");
+    for (i = 0; i != j; i++)
+    {
+        printf("[%d", cycle[i][0]);
+        printf(", %d", cycle[i][1]);
+        printf(", %d] ", cycle[i][2]);
+    }
+    printf("]\n");
+
+    return cycle;
+}
+
+void findAllCycles()
+{
+    int i;
+    int j;
+
+    // 'cycles' stores all the current cycles
+    // as we do not know initially how many cycles there are, we first use 'temp_cycles', and then,
+    // we create 'cycles' with the correct size
+    signed int ***cycles;
+    signed int ***temp_cycles;
+
+    temp_cycles = (signed int ***)calloc(n_input + 1, sizeof(**temp_cycles));
+
+    j = 0;
+    for (i = 0; i < n_input; i++)
+    {
+        if (cycle_id[abs(sequence[i])][0] == 0)
+        {
+            if (sequence[i] > 0)
+                temp_cycles[j] = findCycle(-sequence[i], j + 1);
+            else
+                temp_cycles[j] = findCycle(sequence[i], j + 1);
+            j++;
+        }
+
+        if (cycle_id[abs(sequence[i])][1] == 0)
+        {
+            if (sequence[i] > 0)
+                temp_cycles[j] = findCycle(sequence[i], j + 1);
+            else
+                temp_cycles[j] = findCycle(-sequence[i], j + 1);
+            j++;
+        }
+    }
+
+    cycles = (signed int ***)calloc(j, sizeof(**cycles));
+    for (i = 0; i != j; i++)
+        cycles[i] = temp_cycles[i];
+
+    free(temp_cycles);
+}
+
 void sortReversal()
 {
-    // lr_edges stores the reality edges that go from left to right
-    // rl_edges stores the reality edges that go from right to left
-    signed int *lr_edges;
-    signed int *rl_edges;
 }
 
 int main(int argc, char *argv[])
@@ -301,11 +429,13 @@ int main(int argc, char *argv[])
     readInput(argv[1]);
     printSequence();
 
+    createRealityGraph();
+    printRealityGraph();
+
     createDesireGraph();
     printDesireGraph();
 
-    createRealityGraph();
-    printRealityGraph();
+    findAllCycles();
 
     //revert(0,3);
     //printSequence();
